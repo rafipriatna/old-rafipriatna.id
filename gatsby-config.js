c56df39aa118a0406bef31d43944761f1ef243b8
Rafi Priatna <stormrider136@gmail.com>
2019-08-16 18:13:17 +0700

    fix again!!!

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `rafi-priatna-blog`
+        shortname: `rafipriatna-id`
       }
     },
     {
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -6,7 +6,7 @@ import post from '../templates/post'
 export default class Comments extends Component {
   render() {
     let disqusConfig = {
-      url: `${config.siteUrl+post.path}`,
+      url: `${config.siteUrl+post.id}`,
       identifier: post.id,
       title: post.title,
     }
